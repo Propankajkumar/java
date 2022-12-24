c7ec1a5066f2cf84162944cf16d652b936c6d0e7
Pankaj kumar <104559873+Propankajkumar@users.noreply.github.com>
2022-12-24 13:25:49 +0530

    program of add two numbers

---
+++ addtwo_number.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class addtwo_number {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		System.out.print("enter any two numbers");
+		Scanner s=new Scanner(System.in);
+		
+		
+		a=s.nextInt();
+		b=s.nextInt();
+		
+	c=a+b;
+	
+	System.out.print("som of two numbers: "+c);
+
+	}
+
+}
